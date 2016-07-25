03c10361f60209fed77e9f615af96d326f71b860
ZloiY <NoRFolf1000@mail.ru>
2016-07-25 12:51:10 +0300

    work begin

---
+++ app/src/main/java/com/example/zloiy/customfileexplorer/FileArrayAdapter.java
@@ -0,0 +1,60 @@
+package com.example.zloiy.customfileexplorer;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by ZloiY on 25-Jul-16.
+ */
+public class FileArrayAdapter extends ArrayAdapter<Item> {
+
+    private Context context;
+    private int id;
+    private List<Item> items;
+
+    public FileArrayAdapter(Context context, int textViewresourceId, List<Item> objects){
+        super(context, textViewresourceId, objects);
+        this.context = context;
+        id = textViewresourceId;
+        items = objects;
+    }
+
+    public Item getItem(int i){
+        return items.get(i);
+    }
+
+    public View getView(int postion, View convertView, ViewGroup parent){
+        View v = convertView;
+        if (v==null){
+            LayoutInflater vi = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            v = vi.inflate(id, null);
+        }
+        final Item o = items.get(postion);
+        if (o != null){
+            TextView nameTV = (TextView) v.findViewById(R.id.name);
+            TextView itemSizeTV = (TextView) v.findViewById(R.id.item_size);
+            TextView dateTV = (TextView) v.findViewById(R.id.date);
+            ImageView iconIV = (ImageView) v.findViewById(R.id.icon);
+            String url = "drawable/" + o.getImage();
+            int imageRes = context.getResources().getIdentifier(url, null, context.getPackageName());
+            Drawable image = context.getResources().getDrawable(imageRes);
+            iconIV.setImageDrawable(image);
+
+            if (nameTV != null)
+                nameTV.setText(o.getName());
+            if (itemSizeTV != null)
+                itemSizeTV.setText(o.getData());
+            if (dateTV != null)
+                dateTV.setText(o.getDate());
+        }
+        return  v;
+    }
+}
--- app/src/main/java/com/example/zloiy/customfileexplorer/MainActivity.java
+++ app/src/main/java/com/example/zloiy/customfileexplorer/MainActivity.java
@@ -5,59 +5,88 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import java.io.File;
+import java.text.DateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String path;
+    private File currentDir;
+    private FileArrayAdapter adapter;
+    private ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ListView firstList = (ListView) findViewById(R.id.firstlist);
-        path = "/";
-        if (getIntent().hasExtra("path")){
-            path = getIntent().getStringExtra("path");
-        }
-        setTitle(path);
-        List values = new ArrayList();
-        File dir = new File(path);
-        if (!dir.canRead()){
-            setTitle(getTitle() + " (inaccessible)");
-        }
-        String[] list = dir.list();
-        if (list != null){
-            for (String file:list){
-                if (!file.startsWith("."))
-                    values.add(file);
+        listView = (ListView) findViewById(R.id.listView);
+        currentDir = new File("/");
+        fill(currentDir);
+    }
+
+    private void fill (final File file){
+        File[] dirs = file.listFiles();
+        this.setTitle("Current Dir: "+file.getName());
+        List<Item>dir = new ArrayList<>();
+        List<Item>fls = new ArrayList<>();
+        try{
+            for (File ff: dirs){
+                Date lastModifyDate = new Date(ff.lastModified());
+                DateFormat format = DateFormat.getDateInstance();
+                String date_modify = format.format(lastModifyDate);
+                if (ff.isDirectory()){
+                    File[] fbuf = ff.listFiles();
+                    int buf = 0;
+                    if (fbuf != null){
+                        buf = fbuf.length;
+                    }else buf =0;
+                    String num_items = String.valueOf(buf);
+                    if (buf == 0) num_items = num_items + "item";
+                    else num_items = num_items + " items";
+                    dir.add(new Item(ff.getName(), num_items, date_modify, ff.getAbsolutePath(),"folder_icon"));}
+                else{
+                    fls.add(new Item(ff.getName(), ff.length() + "Bytes", date_modify, ff.getAbsolutePath(), "file_icon"));
+                }
             }
+        }catch (Exception e){
+            e.getStackTrace();
         }
-        Collections.sort(values);
-        ArrayAdapter adapter = new ArrayAdapter<ArrayList<String>>(this, android.R.layout.simple_list_item_1,
-                android.R.id.text1, values);
-        firstList.setAdapter(adapter);
-        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
+        Collections.sort(dir);
+        Collections.sort(fls);
+        dir.addAll(fls);
+        if (!file.getName().equalsIgnoreCase("/"))
+            dir.add(0, new Item("..", "ParentDirectory", "", file.getParent(), "upfolder_icon"));
+        adapter = new FileArrayAdapter(MainActivity.this, R.layout.list_layout, dir);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String filename = (String) parent.getAdapter().getItem(position);
-                if (path.endsWith(File.separator))
-                    filename = path + filename;
-                else filename = path + File.separator + filename;
-                if (new File(filename).isDirectory()){
-                    Intent intent = new Intent(MainActivity.this, MainActivity.class);
-                    intent.putExtra("path", filename);
-                    startActivity(intent);
+                Item item = adapter.getItem(position);
+                if (item.getImage().equalsIgnoreCase("folder_icon")){
+                    currentDir = new File(item.getPath());
+                    fill(currentDir);
                 }else
-                    Toast.makeText(MainActivity.this, filename + "is not a directory", Toast.LENGTH_SHORT).show();
+                    if (item.getImage().equalsIgnoreCase("upfolder_icon")){
+                        if (currentDir.getParent() == null);
+                        else {
+                            currentDir = new File(item.getPath());
+                            fill(currentDir);
+                        }
+                    }else
+                    onFileClick(item);
             }
-        };
-        firstList.setOnItemClickListener(itemClickListener);
+        });
+    }
+
+    private void onFileClick(Item item){
+        Intent intent = new Intent();
+        intent.putExtra("GetPath", currentDir.toString());
+        intent.putExtra("GetFileName", item.getName());
+        setResult(RESULT_OK, intent);
+        finish();
     }
 }
